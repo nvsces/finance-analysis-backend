99106162847fe9ae39507f9c707ba3daf43ddc72
nvsces <nvsces@yandex.ru>
2023-05-01 14:32:31 +0300

    add gSheet config

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from gsheet import GSheet
 
 app = FastAPI()
 
-@app.get("/")
-def about():
-    return 'Rest API service for finance app'
+@app.get("/gSheet")
+async def fetchGSheetData():
+    gSheet = GSheet()
+    data = await gSheet.fetchData()
+    return data
